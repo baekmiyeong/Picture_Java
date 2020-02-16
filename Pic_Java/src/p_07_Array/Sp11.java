57ddff04df0f37a71bdfb0db85fa3e622c88f871
miyou <miyou@DESKTOP-L5ONM5M>
2020-02-16 18:17:23 +0900

    주석 수정~

--- Pic_Java/src/p_07_Array/Sp11.java
+++ Pic_Java/src/p_07_Array/Sp11.java
@@ -6,10 +6,10 @@ public class Sp11 {
 		int [][] test = {
 				{80,60,22,50},{90,55,68,72},{33,75,63}
 		};
-				//배열 전체의 길이를 표시
+			//배열 전체의 길이를 표시
 		for(int i=0; i<test.length; i++) {
 			System.out.println((i+1) + "번째 배열 요소의 길이는 " + test[i].length + "입니다.");
-							//배열 변수 test 각각의 길이를 표시
+									//배열 변수 test 각각의 길이를 표시
 		}
 	}
 
